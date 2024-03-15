3c9eedcaf174729f9a7edc30dda8553af07f1811
Cornel Lupu <cornellupu@hotmail.com>
2024-03-15 15:38:20 -0700

    Add project files.

---
+++ MauiCarouselIssue/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiCarouselIssue
+{
+  public partial class App : Application
+  {
+    public App()
+    {
+      InitializeComponent();
+
+      MainPage = new MainPage();
+    }
+  }
+}
---
+++ MauiCarouselIssue/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using SkiaSharp.Views.Maui.Controls;
+
+namespace MauiCarouselIssue;
+
+public class AllUpVM
+{
+  public required string Content { get; set; }
+
+  public Board Board { get; set; }
+
+  public AllUpVM()
+  {
+    Board = new Board();
+  }
+}
+
+public class PartPage
+{
+  public required string Name { get; set; }
+
+  public required AllUpVM ViewModel { get; set; }
+}
+
+public partial class MainPage : ContentPage
+{
+  public List<PartPage> Parts { get; set; }
+
+  public MainPage()
+  {
+    InitializeComponent();
+    AllUpVM vm = new() { Content = "Common content" };
+
+    Parts = [new PartPage { Name = "First", ViewModel = vm }, new PartPage { Name = "Second", ViewModel = vm }];
+
+    BindingContext = this;
+  }
+}
+
+public class Board : SKCanvasView
+{
+}
+
+
+public class SelectionDataTemplateSelector : DataTemplateSelector
+{
+  public required DataTemplate FirstPartPage { get; set; }
+  public required DataTemplate SecondPartPage { get; set; }
+
+  protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+  {
+    return ((PartPage)item).Name == "First" ? FirstPartPage : SecondPartPage;
+  }
+}
